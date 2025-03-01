05a98f0042764ab46ec93926123edffb900d70bc
virtualformacion <virtualformacion47@gmail.com>
2025-03-01 16:47:42 +0000

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ document.getElementById("emailForm").addEventListener("submit", async function(e
     event.preventDefault();
     var email = document.getElementById("email").value;
 
-
+    // Mostrar el mensaje de espera
+    const alertElement = document.createElement("div");
+    alertElement.id = "loadingAlert";
     alertElement.innerHTML = "Espere un momento, no recargue la página. Estamos consultando su solicitud.";
-
+    alertElement.style.position = "fixed";
+    alertElement.style.top = "50%";
+    alertElement.style.left = "50%";
+    alertElement.style.transform = "translate(-50%, -50%)";
+    alertElement.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+    alertElement.style.color = "white";
+    alertElement.style.padding = "20px";
+    alertElement.style.borderRadius = "10px";
+    document.body.appendChild(alertElement);
 
     // Espera aleatoria entre 10 y 25 segundos antes de hacer la solicitud
     const delay = Math.floor(Math.random() * (25000 - 10000 + 1)) + 10000;
@@ -25,3 +35,4 @@ document.getElementById("emailForm").addEventListener("submit", async function(e
         }
     }, delay);
 });
+
